7f920597d9dec39db949950e43fc3ded3f8cdc87
balabolchik <obordulyak@gmail.com>
2020-11-22 22:24:47 +0200

    pre finish header

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let input = document.getElementById('burger-botton');
+let closeBurgerMenu = document.getElementById('close-burger-menu');
+let i = 0;
+
+
+
+
